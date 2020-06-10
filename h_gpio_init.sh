ce90da385fcb3f51896ead1c08dc49e12e3ccccf
Piotr Michna <pm@piotrmichna.pl>
2020-06-10 18:36:25 +0200

    dodanei funkcji gpio_set serującej pinem wyjściowym

--- h_gpio_init.sh
+++ h_gpio_init.sh
@@ -93,11 +93,39 @@ function gpio_get(){
                 if [ $ret -ne ${GP_STAN[$i]} ] ; then
                    GP_STAN[$i]=$ret
                    mysql -D$DB -u $USER -p$PASS -N -e"UPDATE item SET stan=$ret WHERE id=${GP_ID[$i]};"
+                   log_gp ${GP_GPIO[$i]} $ret "zmiana stanu"
                 fi
+            else
+                log_gp ${GP_GPIO[$i]} 0 "pin nie jest wejsciowy"
             fi
             break
         fi
     done    
+    return $ret
+}
+
+function gpio_set(){
+    local ret=255
+    for (( i=0 ; i<GP_NUM ; i++ )) ; do
+        if [ ${GP_ID[$i]} -eq $1 ] ; then
+        #echo "sens ${GP_NAZ[$i]}"
+            if [ ${GP_DIR[$i]} -eq 1 ] ; then
+                if [ $2 -lt 2 ] ; then
+                    gpio write ${GP_GPIO[$i]} $2
+                    if [ $2 -ne ${GP_STAN[$i]} ] ; then
+                        GP_STAN[$i]=$2
+                        mysql -D$DB -u $USER -p$PASS -N -e"UPDATE item SET stan=$2 WHERE id=${GP_ID[$i]};"
+                         log_gp ${GP_GPIO[$i]} $2 "zmiana stanu"
+                    fi
+                else
+                    log_gp ${GP_GPIO[$i]} 0 "wartosc niedozwolona: $2"
+                fi
+            else
+                log_gp ${GP_GPIO[$i]} 0 "pin nie jest wyjściowy"
+            fi
+            break
+        fi
+    done
     return $ret
 }
 #gpio_init

d55ce4b8e2f999c91becb1a63ecf3d6170617645
Piotr Michna <pm@piotrmichna.pl>
2020-06-10 11:47:38 +0200

    nadanie uprawnien dla pliku h_inig.sh oraz dodanie komentarzy dla blibliotek

--- h_gpio_init.sh
+++ h_gpio_init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #___h_init.sh___
 
-source h_log.sh
+source h_log.sh #funkcje zapisu informacj zdazen do bazy danych
+# log_sys "tresc informacji"
+# log_gp gpio stan "informacja"
+source _test.sh #funkcje testujace czas wykonywania skryptu
+# start_test
+# stop_test
 
 GP_NUM=0
 GP_ID[0]=0

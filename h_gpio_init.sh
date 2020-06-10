944e160db0e64aca98d951a84e3cb7f068127242
Piotr Michna <pm@piotrmichna.pl>
2020-06-10 11:48:47 +0200

    definicja funnkcji inicjujacej porty gpio

--- h_gpio_init.sh
+++ h_gpio_init.sh
@@ -14,3 +14,45 @@ GP_GPIO[0]=0
 GP_DIR[0]=0
 GP_STAN[0]=0
 GP_STAN_ACT[0]=0
+
+function gpio_init(){
+    echo "gpio_mysql_data"
+    start_test
+    local tmp=$(echo "SELECT id FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_ID=( $( for i in $tmp ;do echo $i ;done ) )   
+    GP_NUM=${#GP_ID[@]}
+    
+    tmp=$(echo "SELECT gpio FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_GPIO=( $( for i in $tmp ;do echo $i ;done ) )
+  
+    tmp=$(echo "SELECT dir FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_DIR=( $( for i in $tmp ;do echo $i ;done ) )
+  
+    tmp=$(echo "SELECT stan FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_STAN=( $( for i in $tmp ;do echo $i ;done ) )
+  
+    tmp=$(echo "SELECT stan_act FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_STAN_ACT=( $( for i in $tmp ;do echo $i ;done ) )
+    stop_test
+    echo "gpio_init"
+    start_test
+    for (( i=0 ; i<GP_NUM ; i++ )) ; do
+    if [ ${GP_DIR[$i]} -eq 1 ] ; then # wyjści sterujące
+      if [ ${GP_STAN_ACT[$i]} -eq 1 ] ; then
+        gpio write ${GP_GPIO[$i]} 0 #pulup GND
+      else
+        gpio write ${GP_GPIO[$i]} 1 #pulup Vcc
+      fi
+      gpio mode ${GP_GPIO[$i]} out #kierunek wyjściowy
+    else  # wejście pomiaru
+      if [ ${GP_STAN_ACT[$i]} -eq 1 ] ; then
+        gpio mode ${GP_GPIO[$i]} down #pulup GND
+      else
+        gpio mode ${GP_GPIO[$i]} up #pulup Vcc
+      fi
+      gpio mode ${GP_GPIO[$i]} in #kierunek wejściowy
+    fi
+  done
+  stop_test
+}
+gpio_init

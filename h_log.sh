1bb1da5f8f794d25839d21af8548085ecbb877ea
Piotr Michna <pm@piotrmichna.pl>
2020-06-10 09:37:07 +0200

    definicja funkcji zapisu logow systemowych programu do bazy mysql

--- h_log.sh
+++ h_log.sh
@@ -9,3 +9,9 @@ DBH="homhist"
 #---zmienne globalen daty i czasu
 CUR_DAT=$(date +"%F")
 CUR_TIM=$(date +"%T")
+
+function log_sys(){
+  CUR_DAT=$(date +"%F")
+  CUR_TIM=$(date +"%T")
+  mysql -u $USER -p$PASS -D$DBH -e"INSERT INTO syst (id, dat, tim, opis) VALUES (NULL, '${CUR_DAT}', '${CUR_TIM}', '${1}');"
+}

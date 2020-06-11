5bec1d2bb8f21e6f5cff8cc2c7249f2ded1b1ac4
Piotr Michna <pm@piotrmichna.pl>
2020-06-11 13:09:23 +0200

    dodanie obslugi bledow dla funkcji log_sys()

--- h_log.sh
+++ h_log.sh
@@ -10,10 +10,19 @@ DBH="homhist"
 CUR_DAT=$(date +"%F")
 CUR_TIM=$(date +"%T")
 
-function log_sys(){
-  CUR_DAT=$(date +"%F")
-  CUR_TIM=$(date +"%T")
-  mysql -u $USER -p$PASS -D$DBH -e"INSERT INTO syst (id, dat, tim, opis) VALUES (NULL, '${CUR_DAT}', '${CUR_TIM}', '${1}');"
+function log_sys(){ 
+# log_sys "er" "blędna informacja" 
+# log_sys "poprawna informacja"
+CUR_DAT=$(date +"%F")
+CUR_TIM=$(date +"%T")
+    if [ $1 = "er" ] ; then
+        local ER=1
+        local OPI="$2"
+    else
+        local ER=0
+        local OPI="$1"
+    fi
+    mysql -u $USER -p$PASS -D$DBH -e"INSERT INTO syst (id, dat, tim, opis, er) VALUES (NULL, '${CUR_DAT}', '${CUR_TIM}', '$OPI', $ER);"
 }
 
 function log_gp(){

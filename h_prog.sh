415fd04d2d31512db9a3c15ba6073ec584ebe738
Piotr Michna <pm@piotrmichna.pl>
2020-06-12 14:34:45 +0200

    dolaczenie pliku h_prog_gpio.sh do pliku h_prog.sh

--- h_prog.sh
+++ h_prog.sh
@@ -2,10 +2,14 @@
 #___h_prog.sh___
 
 source h_log.sh #funkcje zapisu informacj zdazen do bazy danych
-# log_sys "er" "blędna informacja" 
+# log_sys "er" "blędna informacja"
 # log_sys "poprawna informacja"
-# log_gp "er" GPIO STAN "blędna informacja" 
-# log_gp GPIO STAN "poprawna informacja" 
+# log_gp "er" GPIO STAN "blędna informacja"
+# log_gp GPIO STAN "poprawna informacja"
+source h_gpio.sh
+# gpio_get_data #pobiera pramtry gpio z mysql
+# gpio_list # wyswietla na ekranie liste dostepnych portow
+# gpio_init # pobiera parametry gpio z mysql i inicjuje gpio
 source _test.sh #funkcje testujace czas wykonywania skryptu
 # start_test
 # stop_test

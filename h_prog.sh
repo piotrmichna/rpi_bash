bc4a463b2e68853f97512d85fcec51058a32caf8
Piotr Michna <pm@piotrmichna.pl>
2020-06-12 15:21:47 +0200

    usuniecie blednego dolaczenia pliku h_prog_gpio.sh

--- h_prog.sh
+++ h_prog.sh
@@ -6,7 +6,7 @@ source h_log.sh #funkcje zapisu informacj zdazen do bazy danych
 # log_sys "poprawna informacja"
 # log_gp "er" GPIO STAN "blędna informacja"
 # log_gp GPIO STAN "poprawna informacja"
-source h_gpio.sh
+source h_prog_gpio.sh
 # gpio_get_data #pobiera pramtry gpio z mysql
 # gpio_list # wyswietla na ekranie liste dostepnych portow
 # gpio_init # pobiera parametry gpio z mysql i inicjuje gpio

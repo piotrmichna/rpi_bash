985e7dcd690a92a5e17efa5dc27c92ea8249cdab
Piotr Michna <pm@piotrmichna.pl>
2020-06-12 14:34:11 +0200

    formatowanie kodu

--- h_prog.sh
+++ h_prog.sh
@@ -34,7 +34,7 @@ function end_prog(){
     PR_NEXT_TIM_CNT=-1
     PR_NEXT_TIM_ELSP=""
     PR_NEXT_PROG_ID=-1
-    PR_ID=-1    
+    PR_ID=-1
     PR_NAZ=""
     # zmienne urzadzen
     PR_ITEM_LP=-1
@@ -43,7 +43,7 @@ function end_prog(){
     PR_ITEM_PAR[0]=0
     PR_ITEM_DELAY[0]=0
     PR_ITEM_CNT[0]=0
-    
+
     if [ $PR_START_NUM -gt 0 ] ; then # sprawdz czy jest kolejny start
         get_next_start
     fi
@@ -52,10 +52,10 @@ function end_prog(){
 function get_prog_item(){
     local tmp=$(echo "SELECT itemid FROM prog_item WHERE progid=$PR_ID ORDER BY lp" | mysql -D$DB -u $USER -p$PASS -N)
     PR_ITEM_ID=( $( for i in $tmp ;do echo $i ;done ) )
-    
+
     tmp=$(echo "SELECT parale FROM prog_item WHERE progid=$PR_ID ORDER BY lp" | mysql -D$DB -u $USER -p$PASS -N)
     PR_ITEM_PAR=( $( for i in $tmp ;do echo $i ;done ) )
-    
+
     tmp=$(echo "SELECT delay_s FROM prog_item WHERE progid=$PR_ID ORDER BY lp" | mysql -D$DB -u $USER -p$PASS -N)
     PR_ITEM_DELAY=( $( for i in $tmp ;do echo $i ;done ) )
     # pobieranie wlasnosci item
@@ -70,14 +70,14 @@ function begin_prog(){
     if [ $PR_ID -gt 0 ] ; then
         local tmp=$(echo "SELECT COUNT(1) FROM prog_item WHERE progid=$PR_ID" | mysql -D$DB -u $USER -p$PASS -N)
         PR_ITEM_NUM=${tmp[0]}
-        if [ $PR_ITEM_NUM -gt 0 ] ; then        
+        if [ $PR_ITEM_NUM -gt 0 ] ; then
             tmp=$(echo "SELECT nazwa FROM prog WHERE id=$PR_ID" | mysql -D$DB -u $USER -p$PASS -N)
             PR_NAZ=${tmp[0]}
             get_prog_item
         else
             log_sys "er"  "wywolanie pustego programu"
             end_prog
-        fi        
+        fi
     else
         log_sys "KONIEC programu [ $PR_NAZ ]"
         end_prog
@@ -88,7 +88,7 @@ function sec_to_str(){
     local T=$1
     local H=$(( T/3600 ))
     local X=$(( H*3600 )) ; T=$(( T-X ))
-    local M=$(( T/60 )) ; X=$(( M*60 )) ; T=$(( T-X )) 
+    local M=$(( T/60 )) ; X=$(( M*60 )) ; T=$(( T-X ))
     local S=$(( T%60 ))
 
     if [ $H -lt 10 ] ; then local tim_str=$(echo "0$H:") ; else local tim_str=$(echo "$H:") ; fi
@@ -101,10 +101,10 @@ function is_time_now(){
     local TIX=`date --date="$1" +%s`
     local TN=$(date +"%T")
     local TNW=`date --date="$TN" +%s`
-    local TE=$(( TIX-TNW ))    
+    local TE=$(( TIX-TNW ))
     PR_NEXT_TIM_SEC=$TE
     PR_NEXT_TIM_ELSP=$( sec_to_str $TE )
-    echo "$TE"   
+    echo "$TE"
 }
 
 function get_next_start(){
@@ -114,19 +114,19 @@ function get_next_start(){
     echo "ilosc startow $PR_START_NUM"
     if [ $PR_START_NUM -gt 0 ] ; then
         tmp=$(echo "SELECT tim FROM start_time WHERE tim>'$TNOW' ORDER BY tim LIMIT 1" | mysql -D$DB -u $USER -p$PASS -N)
-        PR_NEXT_TIM=${tmp[0]}   
+        PR_NEXT_TIM=${tmp[0]}
         tmp=$(echo "SELECT progid FROM start_time WHERE tim>'$TNOW' ORDER BY tim LIMIT 1" | mysql -D$DB -u $USER -p$PASS -N)
         PR_NEXT_PROG_ID=${tmp[0]}
         PR_NEXT_TIM_SEC=$( is_time_now "$PR_NEXT_TIM" )
         PR_NEXT_TIM_CNT=$(( PR_NEXT_TIM_SEC/2 ))
     else
         PR_NEXT_TIM=""
-        R_NEXT_PROG_ID=-1    
+        R_NEXT_PROG_ID=-1
     fi
     #tsn=`date --date="$TNOW" +%s`
     #tss=`date --date="$PR_NEXT_START_TIM" +%s`
     #tse=$(( tss-tsn ))
-    
+
     #xt=$( sec_to_str $tse )
     #echo "pozostały czas do startu to: $xt"
 }
@@ -137,14 +137,14 @@ function wait_for_prog_start(){
             echo "program run"
         else # oczekiwanie na program
             echo "wait for program -> sec: $PR_NEXT_TIM_SEC cnt: $PR_NEXT_TIM_CNT"
-            if [ $PR_NEXT_TIM_SEC -gt 10 ] ; then # ilosc sekund do startu >10 
+            if [ $PR_NEXT_TIM_SEC -gt 10 ] ; then # ilosc sekund do startu >10
                 if [ $PR_NEXT_TIM_CNT -eq 0 ] ; then # akutalizacja ilosci sekund do startu
                     PR_NEXT_TIM_SEC=$( is_time_now "$PR_NEXT_TIM" )
                     PR_NEXT_TIM_CNT=$(( PR_NEXT_TIM_SEC/2 ))
                 else # odliczanie
                     PR_NEXT_TIM_SEC=$(( PR_NEXT_TIM_SEC-1 ))
                     PR_NEXT_TIM_CNT=$(( PR_NEXT_TIM_CNT-1 ))
-                fi                    
+                fi
             else # ilosc sekund do startu <10
                 PR_NEXT_TIM_SEC$( is_time_now "$PR_NEXT_TIM" )
                 if [ $PR_NEXT_TIM_SEC -lt 1 ] ; then
@@ -158,9 +158,9 @@ function wait_for_prog_start(){
     else # brak startow biezacego dnia
         if [ $NEW_DAY -ne $(date +'%-j') ] ; then # oczekiwanie na nastepny dzien
             echo "nowy dzien"
-            get_next_start            
-            NEW_DAY=$(date +"%-j")            
-        fi # ilosc startow    
+            get_next_start
+            NEW_DAY=$(date +"%-j")
+        fi # ilosc startow
     fi # planowane starty
 }
 

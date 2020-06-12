c2ceb77d095a10c60ed02749225468f0fa4bf22b
Piotr Michna <pm@piotrmichna.pl>
2020-06-12 13:39:50 +0200

    definicja funkcji gpio_get_data()

--- h_prog_gpio.sh
+++ h_prog_gpio.sh
@@ -2,10 +2,10 @@
 #___h_prog_gpio.sh___
 
 source h_log.sh #funkcje zapisu informacj zdazen do bazy danych
-# log_sys "er" "blędna informacja" 
+# log_sys "er" "blędna informacja"
 # log_sys "poprawna informacja"
-# log_gp "er" GPIO STAN "blędna informacja" 
-# log_gp GPIO STAN "poprawna informacja" 
+# log_gp "er" GPIO STAN "blędna informacja"
+# log_gp GPIO STAN "poprawna informacja"
 source _test.sh #funkcje testujace czas wykonywania skryptu
 # start_test
 # stop_test
@@ -19,3 +19,39 @@ GP_GPIO[0]=0
 GP_DIR[0]=0
 GP_STAN[0]=0
 GP_STAN_ACT[0]=0
+
+function gpio_get_data(){
+    #echo "gpio_mysql_data"
+    #start_test
+    local tmp=$(echo "SELECT id FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_ID=( $( for i in $tmp ;do echo $i ;done ) )
+    GP_NUM=${#GP_ID[@]}
+
+    tmp=$(echo "SELECT typid FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_TYPID=( $( for i in $tmp ;do echo $i ;done ) )
+
+    tmp=$(echo "SELECT gpio FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_GPIO=( $( for i in $tmp ;do echo $i ;done ) )
+
+    tmp=$(echo "SELECT dir FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_DIR=( $( for i in $tmp ;do echo $i ;done ) )
+
+    tmp=$(echo "SELECT stan FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_STAN=( $( for i in $tmp ;do echo $i ;done ) )
+
+    tmp=$(echo "SELECT stan_act FROM item WHERE en>0 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    GP_STAN_ACT=( $( for i in $tmp ;do echo $i ;done ) )
+
+    #----pobieranie nazw gpio----------
+    for (( i=0 ; i<GP_NUM ; i++ )) ; do
+        tmp=$(echo "SELECT nazwa FROM item WHERE id=${GP_ID[$i]}" | mysql -D$DB -u $USER -p$PASS -N)
+        GP_NAZ[$i]=${tmp[0]}
+    done
+
+    #------pobieranie nazw typow--------
+    for (( i=0 ; i<GP_NUM ; i++ )) ; do
+        tmp=$(echo "SELECT nazwa FROM item_typ WHERE id=${GP_TYPID[$i]}" | mysql -D$DB -u $USER -p$PASS -N)
+        GP_TYPNAZ[$i]=${tmp[0]}
+    done
+    #stop_test
+}

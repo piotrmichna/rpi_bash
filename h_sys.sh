30900e1d8e814c7da25a9f47f9597d9f446f7acb
Piotr Michna <pm@piotrmichna.pl>
2020-06-13 11:56:53 +0200

    definicja funkcji sys_init() pobierajacej parametry z mysql

--- h_sys.sh
+++ h_sys.sh
@@ -11,3 +11,16 @@ SYS_NUM=0
 SYS_ID[0]=0
 SYS_COM[0]=""
 SYS_VAL[0]=0
+
+function sys_init(){
+    local tmp=$(echo "SELECT COUNT(1) FROM cnf WHERE syst=1 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+    SYS_NUM=${tmp[0]}
+    if [ $SYS_NUM -gt 0 ] ; then
+        tmp=$(echo "SELECT id FROM cnf WHERE syst=1 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+        SYS_ID=( $( for i in $tmp ;do echo $i ;done ) )
+        tmp=$(echo "SELECT comm FROM cnf WHERE syst=1 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+        SYS_COM=( $( for i in $tmp ;do echo $i ;done ) )
+        tmp=$(echo "SELECT valu FROM cnf WHERE syst=1 ORDER BY id" | mysql -D$DB -u $USER -p$PASS -N)
+        SYS_VAL=( $( for i in $tmp ;do echo $i ;done ) )
+    fi
+}

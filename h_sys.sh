1f3b5cec22390b79ea0bd170b8ab68f1bde87799
Piotr Michna <pm@piotrmichna.pl>
2020-06-13 11:29:30 +0200

    dolaczenie plkow bibliotecznych

--- h_sys.sh
+++ h_sys.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #___h_sys.sh___
 
+source h_log.sh
+# log_sys "er" "blędna informacja"
+# log_sys "poprawna informacja"
+# log_gp "er" GPIO STAN "blędna informacja"
+# log_gp GPIO STAN "poprawna informacja"

89d3bc9123cf0ea222b41effa2db659c22e89efd
Piotr Michna <pm@piotrmichna.pl>
2020-06-17 18:59:49 +0200

    test instrukcji zmiany szasu/daty na sekundy

--- h_tim.sh
+++ h_tim.sh
@@ -6,7 +6,7 @@ echo "    TIM_A[s]= $TIM_A"
 temp=$(date -d @$TIM_A "+%F %T")
 echo "TIM_A[%F %T]= $temp"
 
-sleep 12
+sleep 2
 
 TIM_B=$(date +"%-s")
 echo "    TIM_B[s]= $TIM_B"
@@ -22,9 +22,17 @@ echo "    TIM_A[s]= $TIM_A"
 echo "  TIM_MID[s]= $TIM_MID"
 echo "    TIM_B[s]= $TIM_B"
 echo "_"
-temp=$(date -d @$TIM_A "+%F %T")
-echo "  TIM_A[%F %T]= $temp"
-temp=$(date -d @$TIM_MID "+%F %T")
-echo "TIM_MID[%F %T]= $temp"
-temp=$(date -d @$TIM_B "+%F %T")
-echo "  TIM_B[%F %T]= $temp"
+tempA=$(date -d @$TIM_A "+%T")
+echo "  TIM_A[%T]= $tempA"
+tempMID=$(date -d @$TIM_MID "+%T")
+echo "TIM_MID[%T]= $tempMID"
+tempB=$(date -d @$TIM_B "+%T")
+echo "  TIM_B[%T]= $tempB"
+
+# czas na sekundy i data na sekundy
+temp=`date --date="$tempA" "+%s"`
+echo " TIM_A -> SEC = $temp"
+echo "        TIM_A = $TIM_A"
+tim=0
+temp=$(date -d @$tim "+%F %T")
+echo "data od 542 -> $temp"

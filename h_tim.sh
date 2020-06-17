a65c0bf607124165b1881796a6e46ff105d9a2f6
Piotr Michna <pm@piotrmichna.pl>
2020-06-17 19:46:01 +0200

    porzadkowanie i uzupelnienie komentarzy w h_tim.sh

--- h_tim.sh
+++ h_tim.sh
@@ -1,41 +1,9 @@
 #!/bin/bash
-#_____h_time.sh
+#_____h_tim.sh
 
-TIM_A=$(date +"%-s")
-echo "    TIM_A[s]= $TIM_A"
-temp=$(date -d @$TIM_A "+%F %T")
-echo "TIM_A[%F %T]= $temp"
-
-sleep 2
-
-TIM_B=$(date +"%-s")
-echo "    TIM_B[s]= $TIM_B"
-temp=$(date -d @$TIM_B "+%F %T")
-echo "TIM_B[%F %T]= $temp"
-echo "________"
-
-TIM_MID=$(( TIM_B-TIM_A ))
-TIM_MID=$(( TIM_MID/2 ))
-TIM_MID=$(( TIM_A+TIM_MID ))
-
-echo "    TIM_A[s]= $TIM_A"
-echo "  TIM_MID[s]= $TIM_MID"
-echo "    TIM_B[s]= $TIM_B"
-echo "_"
-tempA=$(date -d @$TIM_A "+%T")
-echo "  TIM_A[%T]= $tempA"
-tempMID=$(date -d @$TIM_MID "+%T")
-echo "TIM_MID[%T]= $tempMID"
-tempB=$(date -d @$TIM_B "+%T")
-echo "  TIM_B[%T]= $tempB"
-
-# czas na sekundy i data na sekundy
-temp=`date --date="$tempA" "+%s"`
-echo " TIM_A -> SEC = $temp"
-echo "        TIM_A = $TIM_A"
-tim=0
-temp=$(date -d @$tim "+%F %T")
-echo "data od 542 -> $temp"
+# get_tim_form_S 242323
+# get_date_form_S 242323
+# get_s_from_data_time "2000-01-12" lub "2000-01-21 10:32:03"
 
 function get_tim_from_S(){
 	local tim=""

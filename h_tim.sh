690627a9f2f65a6625e02fe96eafb7ff8354e9ae
Piotr Michna <pm@piotrmichna.pl>
2020-06-17 18:09:56 +0200

    test instrukcji konwersji unix sekund na tekst daty i czasu

--- h_tim.sh
+++ h_tim.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
 #_____h_time.sh
+
+TIM_A=$(date +"%-s")
+echo "    TIM_A[s]= $TIM_A"
+temp=$(date -d @$TIM_A "+%F %T")
+echo "TIM_A[%F %T]= $temp"
+
+sleep 12
+
+TIM_B=$(date +"%-s")
+echo "    TIM_B[s]= $TIM_B"
+temp=$(date -d @$TIM_B "+%F %T")
+echo "TIM_B[%F %T]= $temp"
+echo "________"
+
+TIM_MID=$(( TIM_B-TIM_A ))
+TIM_MID=$(( TIM_MID/2 ))
+TIM_MID=$(( TIM_A+TIM_MID ))
+
+echo "    TIM_A[s]= $TIM_A"
+echo "  TIM_MID[s]= $TIM_MID"
+echo "    TIM_B[s]= $TIM_B"
+echo "_"
+temp=$(date -d @$TIM_A "+%F %T")
+echo "  TIM_A[%F %T]= $temp"
+temp=$(date -d @$TIM_MID "+%F %T")
+echo "TIM_MID[%F %T]= $temp"
+temp=$(date -d @$TIM_B "+%F %T")
+echo "  TIM_B[%F %T]= $temp"

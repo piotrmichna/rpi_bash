53187b1ce2e92d6c773876c2449583304639a3f0
Piotr Michna <pm@piotrmichna.pl>
2020-06-19 12:06:47 +0200

    definicaj fun. pl_init() inicjujacej dane trybu plukania

--- h_prog.sh
+++ h_prog.sh
@@ -15,3 +15,39 @@ source h_test.sh #funkcje testujace czas wykonywania skryptu
 # stop_test
 
 TNOW=$(date +"%T") #zminna zawierający aktualny czas
+TRYB=-1
+
+EZ_BUZ_TIM=0         #czas napelnienia buzaw
+PM_PMP_TIM=0    #czas oproznienia buzaw
+PL_START_N=0          # ilosc plukan na starcie
+PL_STOP_N=0            #ilosc plukani na stop
+PL_STAN=-1                 # stan funkcji plukania
+EZ_BUZ_CNT=0
+PMP_BUZ_CNT=0
+PL_START_CNT=0
+PL_STOP_CNT=0
+
+function pl_init(){
+    local tmp=$(echo "SELECT valu FROM prog WHERE comm='ez_buz_time'" | mysql -D$DB -u $USER -p$PASS -N)
+    EZ_BUZ_TIM=${tmp[0]}
+    tmp=$(echo "SELECT valu FROM prog WHERE comm='pmp_buz_time'" | mysql -D$DB -u $USER -p$PASS -N)
+    PMP_BUZ_TIM=${tmp[0]}
+    tmp=$(echo "SELECT valu FROM prog WHERE comm='pl_start_n'" | mysql -D$DB -u $USER -p$PASS -N)
+    PL_START_N=${tmp[0]}
+    tmp=$(echo "SELECT valu FROM prog WHERE comm='pl_stop_n'" | mysql -D$DB -u $USER -p$PASS -N)
+    PL_STOP_N=${tmp[0]}
+    PL_STAN=0
+    mysql -D$DB -u $USER -p$PASS -N -e"UPDATE prog SET valu=$PL_STAN WHERE comm='pl_stan';"
+}
+
+while [ 1 ] ; do
+    if [ $TRYB -eq -1 ] ; then
+        mysql -D$DB -u $USER -p$PASS -N -e"UPDATE prog SET info='Inicjalizacja systemu' WHERE comm='tryb_info';"
+        gpio_init
+        pl_init
+        TRYB=0
+        mysql -D$DB -u $USER -p$PASS -N -e"UPDATE prog SET valu=$TRYB WHERE comm='tryb';"
+    fi
+    echo "tryb=$TRYB PL_START_N=$PL_START_N"
+     sleep 1
+done
